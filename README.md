f2b9d0bb342b26ba0bf653c9f05684a5a003db11
Juliano Sirtori <julianosirtori@gmail.com>
2020-07-15 22:45:50 -0300

    UPDATE README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+# CORS-anywhere
+
 https://www.npmjs.com/package/cors-anywhere
+
+
+1. Install Dependencies
+```
+yarn install
+```
+
+2. To Run
+```
+yarn serve
+```
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
+let host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+let port = process.env.PORT || 8080;
  
-var cors_proxy = require('cors-anywhere');
+let cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
